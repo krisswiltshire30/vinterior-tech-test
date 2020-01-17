b57239122f96ebd6e28736af04b705e932e31e7e
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2020-01-17 22:16:58 +0000

    Should remove 0.75 from each chair if 2 or more chairs in basket - Passed

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Checkout
-  attr_reader :basket, :total
-  def initialize(basket = [], total = 0)
+  attr_reader :promotional_rules, :basket, :total
+  def initialize(promotional_rules = [], basket = [], total = 0)
+    @promotional_rules = promotional_rules
     @basket = basket
     @total = total
   end
@@ -15,6 +16,21 @@ class Checkout
     @basket.each do |item|
       @total += item[:price]
     end
+    promotion_checker
     @total
+  end
+
+  def promotion_checker
+    @promotional_rules.each do |promo|
+      multibuy(promo) if promo[:type] == 'multibuy'
+    end
+  end
+
+  def multibuy(promo)
+    @basket.each do |item|
+      if item[:product_code] == promo[:product_code]
+        item[:price] = promo[:price]
+      end
+    end
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,24 +2,28 @@
 
 require_relative '../lib/checkout.rb'
 
+promotions = [
+  { type: 'multibuy', product_code: 0o01, item_amount: 2, price: 8.50 }
+]
+
 products = [
-  { product_code: 001, name: 'Very Cheap Chair', price: 9.25 },
-  { product_code: 002, name: 'Little table', price: 45.00 },
-  { product_code: 003, name: 'Funky light', price: 19.95 }
+  { product_code: 0o01, name: 'Very Cheap Chair', price: 9.25 },
+  { product_code: 0o02, name: 'Little table', price: 45.00 },
+  { product_code: 0o03, name: 'Funky light', price: 19.95 }
 ]
 
 describe 'Checkout' do
   before(:each) do
-    @co = Checkout.new
+    @co = Checkout.new(promotions)
   end
-  context 'basket' do
+  context 'Basket' do
     it 'Should add an item to the basket' do
       @co.scan(products[0])
       expect(@co.basket[0]).to eq(products[0])
     end
   end
 
-  context 'total' do
+  context 'Total' do
     it 'Should have a starting basket total of zero' do
       @co = Checkout.new
       expect(@co.total).to eq(0)
@@ -29,6 +33,15 @@ describe 'Checkout' do
       @co.scan(products[0])
       @co.scan(products[1])
       expect(@co.total).to eq 54.25
+    end
+  end
+
+  context 'Promotions' do
+    it 'Should remove 0.75 from each chair if 2 or more chairs in basket' do
+      @co.scan(products[0])
+      @co.scan(products[0])
+      @co.scan(products[2])
+      expect(@co.total).to eq(36.95)
     end
   end
 end
